3e260fe4cade9b2cae5b28ca6dcd0fc9f3465832
Sam Low-Chappell <sam@hackeryou.com>
2020-02-19 13:11:14 -0500

    updated dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
 		<div className="App">
 			<Layout>
 				<p>Hello World</p>
+				<Modal />
 			</Layout>
 		</div>
 	);
--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -1,7 +1,29 @@
 import React, { FC } from 'react';
 
 const Modal: FC = () => {
-	return <p>Modal Component</p>;
+	return (
+		<div>
+			<h2>Welcome to Match Game!</h2>
+			<h3>Rules:</h3>
+			<p>
+				Here are the rules ... Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum eos
+				accusantium consectetur, fugiat laudantium totam quibusdam voluptatem illum aut adipisci laborum nisi
+				fugit temporibus nostrum! Sequi ex quae inventore? Excepturi.
+			</p>
+			<h3>Choose your deck:</h3>
+			<div>
+				<button>Deck 1</button>
+				<button>Deck 2</button>
+				<button>Deck 3</button>
+			</div>
+			<h3>Choose your difficulty:</h3>
+			<div>
+				<button>Easy</button>
+				<button>Medium</button>
+				<button>Hard</button>
+			</div>
+		</div>
+	);
 };
 
 export default Modal;
